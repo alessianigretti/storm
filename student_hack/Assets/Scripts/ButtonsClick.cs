f210aa934650a7f9bff45db3f381c39d5e318848
Alessia Nigretti <alessia.nigretti@gmail.com>
2017-03-11 04:57:32 +0000

    Completed main menu

---
+++ student_hack/Assets/Scripts/ButtonsClick.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsClick : MonoBehaviour
+{
+    public GameObject mainAudio, pressAudio;
+
+    
+}
---
+++ student_hack/Assets/Scripts/ButtonsInteraction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonsInteraction : MonoBehaviour
+{
+    public GameObject anx, depr, panic, exit;
+    public GameObject mainAudio, hoverAudio, pressAudio;
+
+    public void HighlightAnx()
+    {
+        anx.SetActive(true);
+        hoverAudio.SetActive(true);
+    }
+
+    public void HighlightDepr()
+    {
+        depr.SetActive(true);
+        hoverAudio.SetActive(true);
+    }
+
+    public void HighlightPanic()
+    {
+        panic.SetActive(true);
+        hoverAudio.SetActive(true);
+    }
+
+    public void HighlightExit()
+    {
+        exit.SetActive(true);
+        hoverAudio.SetActive(true);
+    }
+
+    public void Unhighlight()
+    {
+        anx.SetActive(false);
+        depr.SetActive(false);
+        panic.SetActive(false);
+        exit.SetActive(false);
+        hoverAudio.SetActive(false);
+    }
+
+    public void AnxButtonPressed()
+    {
+        Debug.Log("jdisjj");
+        mainAudio.SetActive(false);
+        pressAudio.SetActive(true);
+    }
+}
